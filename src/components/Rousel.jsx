a2916526b6101512838c9f7d78d2ecc80388e79c
143zzy <jc12zaragoza@gmail.com>
2024-01-21 15:58:09 +0800

    Carousel effect
    Swiper.js Carousel

--- src/components/Rousel.jsx
+++ src/components/Rousel.jsx
@@ -1,22 +1,55 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import React from "react";
-import Carousel from "react-bootstrap/Carousel";
-import "swiper/css";
+import "swiper/css/bundle";
+import { A11y, EffectCoverflow, EffectFade, Navigation, Pagination, Scrollbar } from 'swiper/modules';
 function Rousel() {
   return (
     <>
-      <div className="rouselCont">
+      <div className="swiper-container">
         <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
+          modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]}
+          effect="coverflow"
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          loop= "true"
+          spaceBetween={20}
+          slidesPerView={2}
+          coverflowEffect={{
+            rotate:0,
+            stretch:80,
+            depth:200,
+            modifier:1,
+            slideShadows:false,
+            }}
           onSlideChange={() => console.log("slide change")}
           onSwiper={(swiper) => console.log(swiper)}
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          ...
+          <div className="swiper" id="swiper-1">
+            <div className="swiper-wrapper">
+              <SwiperSlide>
+                <img src="src\assets\1.jpg" id="img1" alt="img1" loading="lazy"></img>
+                <div class="swiper-lazy-preloader"></div>
+                <div className="overlay"></div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="src\assets\2.jpg" id="img2" alt="img2" loading="lazy"></img>
+                <div class="swiper-lazy-preloader"></div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="src\assets\3.jpg" id="img3" alt="img3" loading="lazy"></img>
+                <div class="swiper-lazy-preloader"></div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="src\assets\4.jpg" id="img4" alt="img4" loading="lazy"></img>
+                <div class="swiper-lazy-preloader"></div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="src\assets\1.jpg" id="img1" alt="img1" loading="lazy"></img>
+                <div class="swiper-lazy-preloader"></div>
+              </SwiperSlide>
+            </div>
+          </div>
         </Swiper>
       </div>
     </>
